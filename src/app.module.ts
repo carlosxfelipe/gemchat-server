1b88f58af63acfbea6f995669d48f4d2caf8475f
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-06-02 21:22:35 -0300

    feat: add Gemini module with controller and service; remove AppController and AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GeminiModule } from './gemini/gemini.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [GeminiModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
