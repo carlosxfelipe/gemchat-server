c96d25c8276bccadb9b445b18bbaa890f62dfb8c
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-06-06 23:11:08 -0300

    feat: add GoogleGenAI integration to GeminiService for basic prompt handling

--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { BasicPromptDto } from './dtos/basic-prompt.dto';
+import { GoogleGenAI } from '@google/genai';
 
 @Injectable()
 export class GeminiService {
-  basicPrompt(basicPromptDto: BasicPromptDto) {
-    console.log('Basic Prompt:', basicPromptDto);
-    return basicPromptDto;
+  private ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+  async basicPrompt(basicPromptDto: BasicPromptDto) {
+    const systemInstruction =
+      'Responda sempre em português e utilize formatação Markdown quando apropriado. Limite a resposta a 1 ou 2 parágrafos curtos, mantendo a objetividade e clareza.';
+
+    const response = await this.ai.models.generateContent({
+      model: 'gemini-2.0-flash',
+      contents: basicPromptDto.prompt,
+      config: {
+        systemInstruction: systemInstruction,
+      },
+    });
+
+    return response.text;
   }
 }
